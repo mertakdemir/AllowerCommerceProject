b0bdfbde5a9e7b866f60442f4b30a29b26789a31
AhmedSaidTuncpinar <116199239+AhmedSaidTuncpinar@users.noreply.github.com>
2023-01-26 15:23:39 +0100

    message

--- src/test/java/techproed/pages/HomePage.java
+++ src/test/java/techproed/pages/HomePage.java
@@ -1,4 +1,13 @@
 package techproed.pages;
 
+import org.openqa.selenium.support.PageFactory;
+import techproed.utilities.Driver;
+
 public class HomePage {
+
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
 }
