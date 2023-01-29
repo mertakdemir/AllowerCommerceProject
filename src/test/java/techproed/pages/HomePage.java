80ac3acdca4077e5ccef06cab49e3c59b5bf04a7
zeynep395 <zynpefe@gmail.com>
2023-01-29 14:21:13 +0000

    localden branch e update

--- src/test/java/techproed/pages/HomePage.java
+++ src/test/java/techproed/pages/HomePage.java
@@ -1,43 +1,43 @@
 package techproed.pages;
 
 
-<<<<<<< HEAD
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import org.openqa.selenium.WebDriver;
-=======
->>>>>>> master
+
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> master
+
+
+
 import org.openqa.selenium.support.PageFactory;
 import techproed.utilities.Driver;
 
 public class HomePage {
 
-<<<<<<< HEAD
+
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "//span[.='Sign In']")
     public WebElement signInButton1;
-=======
-    public HomePage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+
+
+
+
 
     @FindBy(xpath = "//input[@aria-label='Search']")
     public WebElement searchBox;
 
->>>>>>> master
+
 
     @FindBy(xpath = "//span[.='Register']")
     public WebElement registerButton;
@@ -46,10 +46,10 @@ public class HomePage {
     public WebElement myAccountButton;
 
 
-<<<<<<< HEAD
-}
 
-=======
+
+
+
 
 
 
@@ -59,7 +59,7 @@ public class HomePage {
 
 
     }
->>>>>>> master
+
 
 
 
